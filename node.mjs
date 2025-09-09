c9f00cadae727b4ea428e74169c8020fde67602b
Lyumih <Lyumih@users.noreply.github.com>
2025-09-09 07:19:59 +0000

    Deploying to gh-pages from @ Lyumih/our@fc4443b17f99ecd4ba12e05d6c2f9f8783816ad1 🚀

--- node.mjs
+++ node.mjs
@@ -9349,6 +9349,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
